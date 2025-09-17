22a3ace1a499731fc80ccecc06c2110e7b83359e
Valentin Vasilev <valentin.v1997@gmail.com>
2025-09-17 18:20:44 +0300

    Moved code to solution branch

--- code/src/main/java/com/contoso/adviser/model/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.contoso.adviser.model;
-
-public enum Gender {
-    MALE, FEMALE
-}
--- code/src/main/java/com/contoso/adviser/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.contoso.adviser.repository;
-
-import com.contoso.adviser.model.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> {
-}
